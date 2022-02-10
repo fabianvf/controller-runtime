27955fb31976273fa80513c09c25dcac9b646db9
Fabian von Feilitzsch <fabian@fabianism.us>
2022-02-10 16:27:16 -0500

    i guess it works?

--- pkg/cache/internal/cache_reader.go
+++ pkg/cache/internal/cache_reader.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/selection"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clusters"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -61,8 +62,11 @@ func (c *CacheReader) Get(_ context.Context, key client.ObjectKey, out client.Ob
 	if c.scopeName == apimeta.RESTScopeNameRoot {
 		key.Namespace = ""
 	}
+	key.Name = clusters.ToClusterAwareKey(out.GetClusterName(), key.Name)
+
 	storeKey := objectKeyToStoreKey(key)
 	fmt.Println("storeKey", storeKey)
+	fmt.Println("c.Indexer.ListKeys()", c.indexer.ListKeys())
 
 	// Lookup the object from the indexer cache
 	obj, exists, err := c.indexer.GetByKey(storeKey)
