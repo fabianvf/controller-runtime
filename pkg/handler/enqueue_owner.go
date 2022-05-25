2c01bd98252a92db92ab7e1ea7ea415a0af75475
Andy Goldstein <andy.goldstein@redhat.com>
2022-05-25 14:43:34 -0400

    more kcp plumbing
    Signed-off-by: Andy Goldstein <andy.goldstein@redhat.com>

--- pkg/handler/enqueue_owner.go
+++ pkg/handler/enqueue_owner.go
@@ -19,6 +19,7 @@ package handler
 import (
 	"fmt"
 
+	"github.com/kcp-dev/logicalcluster"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -135,9 +136,14 @@ func (e *EnqueueRequestForOwner) getOwnerReconcileRequest(object metav1.Object, 
 		// object in the event.
 		if ref.Kind == e.groupKind.Kind && refGV.Group == e.groupKind.Group {
 			// Match found - add a Request for the object referred to in the OwnerReference
-			request := reconcile.Request{ObjectKey: client.ObjectKey{NamespacedName: types.NamespacedName{
-				Name: ref.Name,
-			}}}
+			request := reconcile.Request{
+				ObjectKey: client.ObjectKey{
+					Cluster: logicalcluster.From(object),
+					NamespacedName: types.NamespacedName{
+						Name: ref.Name,
+					},
+				},
+			}
 
 			// if owner is not namespaced then we should set the namespace to the empty
 			mapping, err := e.mapper.RESTMapping(e.groupKind, refGV.Version)
